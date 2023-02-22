0c943d5b00c31b45a24977f9e85edfe869bbdd1b
Marco <marco.hl9antonio@gmail.com>
2023-02-22 16:40:25 -0500

    creacion del proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Portada from './Componentes/Portada';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Portada></Portada>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Portada.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Portada(){
+    return(
+        <div>
+            <div style={{ backgroundImage: "url(/images/basketball.jpeg)" }}>
+            Hello World
+            </div>
+            <img src="./images/imagen1.png" alt="foto_Portada"/>
+            <p> aaimg</p>
+        </div>
+    )
+    
+}
+export default Portada;
